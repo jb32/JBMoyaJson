dc83e8a6bade4904cbce19fdb1334fdb5bab9139
jb32 <2062964913@qq.com>
2018-04-21 17:40:35 +0800

    framework

---
+++ JBMoyaJson/Sources/JBMoyaJson.h
@@ -0,0 +1,19 @@
+//
+//  JBMoyaJson.h
+//  JBMoyaJson
+//
+//  Created by 薛 靖博 on 2018/4/21.
+//  Copyright © 2018年 JB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JBMoyaJson.
+FOUNDATION_EXPORT double JBMoyaJsonVersionNumber;
+
+//! Project version string for JBMoyaJson.
+FOUNDATION_EXPORT const unsigned char JBMoyaJsonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JBMoyaJson/PublicHeader.h>
+
+
--- Podfile
+++ Podfile
@@ -2,7 +2,7 @@
 
 platform :ios, '8.0'
 
-target 'MoyaJsonExt' do
+target 'JBMoyaJson' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   
   use_frameworks!
